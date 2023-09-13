ddef88b17ae738a8b8acf35f33224e1788bfc0d1
vandodev <vandoaparecido@hotmail.com>
2023-09-13 14:46:59 -0300

    :robot: :rocket: :robot: Separando VideoInputForm em componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
-import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import { Github, Wand2 } from 'lucide-react'
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from './components/video-input-form';
+
 export function App() {
 
   return (
@@ -46,33 +48,8 @@ export function App() {
       </div>
 
       <aside className="w-80 space-y-6">
-        <form className="space-y-6">
-          <label
-            htmlFor="video"
-            className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-          >
-            <FileVideo className="w-4 h-4" />
-            Selecione um vídeo
-          </label>
 
-          <input type="file" id="video" accept="video/mp4" className="sr-only" />
-
-          <Separator />
-
-          <div className="space-y-2">
-            <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-            <Textarea
-              id="transcription_prompt"
-              className="h-20 leading-relaxed resize-none"
-              placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-            />
-          </div>
-
-          <Button type="submit" className="w-full">
-            Carregar video
-            <Upload className="w-4 h-4 ml-2" />
-          </Button>
-        </form>
+        <VideoInputForm />
 
         <Separator />
 
