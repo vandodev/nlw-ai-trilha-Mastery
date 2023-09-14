c04fcb415e5bfc297590ef3dedabd19294ceabae
vandodev <vandoaparecido@hotmail.com>
2023-09-14 16:09:10 -0300

    :robot: :rocket: :robot: Convertendo vídeo em áudio com ffmpeg

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -3,8 +3,9 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-// import { ChangeEvent } from "react";
 import { ChangeEvent, useState, useMemo, FormEvent, useRef } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 
 export function VideoInputForm(){
@@ -32,6 +33,45 @@ export function VideoInputForm(){
         return URL.createObjectURL(videoFile)
       }, [videoFile])
 
+      async function convertVideoToAudio(video: File) {
+        console.log('Convert started.')
+    
+        const ffmpeg = await getFFmpeg()
+    
+        await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+    
+        // ffmpeg.on('log', log => {
+        //   console.log(log)
+        // })
+    
+        ffmpeg.on('progress', progress => {
+          console.log('Convert progress: ' + Math.round(progress.progress * 100))
+        })
+    
+        await ffmpeg.exec([
+          '-i',
+          'input.mp4',
+          '-map',
+          '0:a',
+          '-b:a',
+          '20k',
+          '-acodec',
+          'libmp3lame',
+          'output.mp3'
+        ])
+    
+        const data = await ffmpeg.readFile('output.mp3')
+    
+        const audioFileBlob = new Blob([data], { type: 'audio/mp3' })
+        const audioFile = new File([audioFileBlob], 'output.mp3', {
+          type: 'audio/mpeg'
+        })
+    
+        console.log('Convert finished.')
+    
+        return audioFile
+      }
+
       async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
     
@@ -40,6 +80,12 @@ export function VideoInputForm(){
         if (!videoFile) {
           return
         }
+
+         // converter o video em áudio
+        const audioFile = await convertVideoToAudio(videoFile)
+
+        console.log("Convert finished")
+        return audioFile
       }
     
 
