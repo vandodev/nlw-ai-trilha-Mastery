80344b4362a08b1ed572b84bda0d33768a9fc51f
vandodev <vandoaparecido@hotmail.com>
2023-09-15 20:43:47 -0300

    :robot: :rocket: :robot: Buscandp prompt do banco do dados

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
 
@@ -56,15 +57,7 @@ export function App() {
         <form className="space-y-6">
           <div className="space-y-2">
             <Label>Prompt</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Selecione um prompt..." />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="title">Título do YouTube</SelectItem>
-                <SelectItem value="description">Descrição do YouTube</SelectItem>
-              </SelectContent>
-            </Select>
+            <PromptSelect  />
           </div>
 
           <div className="space-y-2">
